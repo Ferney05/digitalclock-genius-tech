e0216dc2e6b0931fb16cd8bdb6ac50d065b2c23a
Ferney Barbosa <barbosaferney05@gmail.com>
2022-11-26 21:11:45 -0500

    Subiendo proyecto

--- verificando-periodo.js
+++ verificando-periodo.js
@@ -1,10 +1,18 @@
 
 export const verificarTiempo = () => {
     const tarde_mañana = document.querySelector('[data-tarde_mañana]');
+    const goodDay = document.querySelector('[dataGoodDay]');
+
     const tiempoActual = new Date();
     const hora = tiempoActual.getHours();
 
     let jornada = hora >= 12 ? 'PM' : 'AM';
     tarde_mañana.innerHTML = jornada;
+
+    let timeGood = hora >= 12 ? 'Buenas tardes😉' : 'Buenos días😉';
+    let goodNoche = hora > 7 || hora < 11 ? 'Buenas noches😉' : 'Buenos días😉';
+
+    goodDay.innerHTML = timeGood;
+    goodDay.innerHTML = goodNoche;
 
 }
